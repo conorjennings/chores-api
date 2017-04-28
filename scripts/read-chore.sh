fea18390804ffadf0199d66a2f667ad39972fa1b
“conorjennings” <conorjennings@gmail.com>
2017-04-28 16:40:12 -0400

    Commiting a CURL script for GET listing of all chores

--- scripts/read-chore.sh
+++ scripts/read-chore.sh
@@ -1,2 +1,3 @@
-curl --include --request GET http://localhost:4741/chores \
-  --header "Content-Type: application/json" \
+  curl --include --request GET http://localhost:4741/chores \
+    --header "Content-Type: application/json" \
+    --header "Authorization: Token token=BAhJIiU2NWNmYzNlNWFhYWE0OGExYWZiMjg0NWYxZTcyNGQ0NgY6BkVG--29f5d76c3e3a81c0ac961110e0dbca5bd6f61450" \
