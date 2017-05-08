887a25eb441502de82995c9210bd3698aba81a27
“conorjennings” <conorjennings@gmail.com>
2017-05-08 18:42:32 -0400

    Altered Chores routes file to ensure that all 3 attributes are mandatory and validated on the back end using presence: true

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -1,3 +1,7 @@
 class Chore < ApplicationRecord
   belongs_to :user  # Resolves the 1:N relationship between user and chores
+  # Put validations here for mandatory fields without having to do a table alter.
+  #  http://guides.rubyonrails.org/active_record_validations.html
+  # http://guides.rubyonrails.org/active_record_validations.html#presence
+  validates :task, :priority, :due_on, presence: true
 end
